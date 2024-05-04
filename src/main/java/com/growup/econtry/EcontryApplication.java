d00d80897e64c3fd5a38d74dad29c84135f42fa4
cje206 <projec206@gmail.com>
2024-05-04 10:02:02 +0900

    first commit

---
+++ src/main/java/com/growup/econtry/EcontryApplication.java
@@ -0,0 +1,13 @@
+package com.growup.econtry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcontryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcontryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/growup/econtry/EcontryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.growup.econtry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcontryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
