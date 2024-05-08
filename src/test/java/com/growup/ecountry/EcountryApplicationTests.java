9e579662ed2447b8f2869b426810e8bab7be0bbd
cje206 <projec206@gmail.com>
2024-05-08 11:19:45 +0900

    ãin: 초기세팅

---
+++ src/test/java/com/growup/ecountry/EcountryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.growup.ecountry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcountryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
