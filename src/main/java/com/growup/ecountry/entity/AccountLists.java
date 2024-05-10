972a7603d507adc0d57089e79d6a11ae2935a1ce
cje206 <projec206@gmail.com>
2024-05-10 14:03:09 +0900

    feat: 계좌 테이블 생성

---
+++ src/main/java/com/growup/ecountry/entity/AccountLists.java
@@ -0,0 +1,42 @@
+package com.growup.ecountry.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AccountLists {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TINYINT(1) default 1", nullable = false)
+    private boolean division;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Double interest;
+
+    @Column(nullable = false)
+    private Integer dueDate;
+
+    @Column(columnDefinition = "TINYINT(1) default 1", nullable = false)
+    private boolean available;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id", insertable=false, updatable=false)
+    private Countries countries;
+
+    @Column(name = "country_id")
+    private Long countryId;
+
+    public AccountLists(Long countryId) {
+        this.countryId = countryId;
+    }
+}
