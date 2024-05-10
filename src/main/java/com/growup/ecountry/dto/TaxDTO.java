2b2c6560b2dc7cc8ff6b1e23d4d7bc4cf8181bdd
Hyunkyung-Nam <gusrud13579@gmail.com>
2024-05-10 12:21:19 +0900

    feat:세금규칙 등록/조회/삭제/수정

--- src/main/java/com/growup/ecountry/dto/TaxDTO.java
+++ src/main/java/com/growup/ecountry/dto/TaxDTO.java
@@ -1,2 +1,16 @@
-package com.growup.ecountry.dto;public class TaxDTO {
+package com.growup.ecountry.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class TaxDTO {
+    private Long id;
+    private String name;
+    private Integer division;
+    private Double tax;
+    private Long countryId;
 }
